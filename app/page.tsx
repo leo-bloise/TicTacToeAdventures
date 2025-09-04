d680b90a50d675886170bae06622333f28c3cc1f
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-09-04 09:50:54 -0300

    fix: remove useless import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import Board from "@/features/game/components/Board";
-import { OMark } from "@/features/game/components/OMark";
 import Scoreboard from "@/features/game/components/Scoreboard";
-import XMark from "@/features/game/components/XMark";
 import { GameContextProvider } from "@/features/game/stores/GameContext";
 
 export default function Page() {
