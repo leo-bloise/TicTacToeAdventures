1c1f98b8e9292e843b3dc81183e7250195652ee8
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-09-06 11:10:58 -0300

    wip: implement middleware for route protection based on authentication token

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+const publicRoutes = [
+    '/auth',
+    '/auth/api',
+    '/auth/sign-in',
+    '/register'
+];
+
+export async function middleware(request: NextRequest) {
+    const { get } = await cookies();
+
+    const path = request.nextUrl.pathname;
+    
+    const isPublicRoute = publicRoutes.includes(path);
+
+    const tokenCookie = get('token');
+
+    if(isPublicRoute && tokenCookie) {
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    if(!isPublicRoute && !tokenCookie) {
+        return NextResponse.redirect(new URL('/register', request.nextUrl))
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
